c9071c90c14a6f7d17067fad0d4d60f945a14c7d
Hoàng Đức <macbook@macbooks-MacBook-Pro.local>
2022-11-14 13:49:32 +0700

    First Commit

---
+++ Instagram-clone/Controllers/Main Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-clone
+//
+//  Created by Hoàng Đức on 14/11/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram-clone/Controllers/Main Tabs/NofiticationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NofiticationViewController.swift
+//  Instagram-clone
+//
+//  Created by Hoàng Đức on 14/11/2022.
+//
+
+import UIKit
+
+class NofiticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
